5792b3c5c978da58d17d2b6fc8c66d165c91c1a6
haydencase0 <haymonkey04@gmail.com>
2026-01-22 09:28:39 -0700

    js

--- Week3/ponder/responsive.js
+++ Week3/ponder/responsive.js
@@ -1,1 +1,12 @@
-"use classlist.add to make nav work"
+const nav = document.querySelector('nav');
+const toggleButton = document.querySelector('.menu-btn');
+
+toggleButton.addEventListener('click', () =>{
+    nav.classList.toggle('active');
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 700) {
+        nav.classList.remove('active');
+    }
+});
