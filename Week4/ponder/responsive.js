9c0d274cf405561fe5659385e29a87a3481c7ee4
haydencase0 <haymonkey04@gmail.com>
2026-01-27 10:00:59 -0700

    new ponder

---
+++ Week4/ponder/responsive.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector('nav');
+const toggleButton = document.querySelector('.menu-btn');
+
+toggleButton.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    toggleButton.classList.toggle('open');
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 700) {
+        nav.classList.remove('active');
+    }
+});
