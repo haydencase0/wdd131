f884eccec8c05fb80b6174dd8c7d92f35af787c4
haydencase0 <haymonkey04@gmail.com>
2026-01-27 10:29:53 -0700

    transition

--- Week4/ponder/responsive.js
+++ Week4/ponder/responsive.js
@@ -4,6 +4,11 @@ const toggleButton = document.querySelector('.menu-btn');
 toggleButton.addEventListener('click', () => {
     nav.classList.toggle('active');
     toggleButton.classList.toggle('open');
+    //if(toggleButton.classList.toggle('open')){
+    //    toggleButton.classList.toggle('open');
+    //} else {
+    //    toggleButton.classList.toggle('close');
+    //}
 });
 
 window.addEventListener('resize', () => {
