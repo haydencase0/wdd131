62449294fea1424684a33505ec625169b0654b0c
haydencase0 <haymonkey04@gmail.com>
2026-02-10 10:03:10 -0700

    more code

--- Week6/ponder/arrays.js
+++ Week6/ponder/arrays.js
@@ -3,7 +3,18 @@ const students = [
     {last: 'Andrus', first: 'Aaron'},
     {last: 'Masa', first:'Manny'},
     {last: 'Tanda', first: 'Tamanda'}
-];
+]; // give student a grade
+
+students.foreach((student, index) => {
+
+});
+
+const ul = queryS
+let html = '';
+words.forEach((word) =>{
+    html+= `<li>${word}</li>`;
+});
+ul.innerHTML = html;
 
 function convert(grade) {
     switch (grade){
