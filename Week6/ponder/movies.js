e32262898505ad77e02e5a620b44a5616bf72d56
haydencase0 <haymonkey04@gmail.com>
2026-02-12 09:43:22 -0700

    once again

--- Week6/ponder/movies.js
+++ Week6/ponder/movies.js
@@ -45,16 +45,6 @@ const movies = [
     }
 ];
 
-/* 
-<h2>Spider-Man: Into the Spider-Verse</h2>
-<img src="https://wddbyui.github.io/wdd131/images/spiderman.png" alt="Miles Morales swinging through the city">
-<p><strong>Release Date:</strong> Dec 14, 2018</p>
-<p><strong>Recommended Age:</strong> 10+</p>
-<p><strong>Genre:</strong> Action/Adventure</p>
-<p><strong>Rating:</strong> <span>⭐⭐⭐⭐⭐</span></p>
-<p id='desc'>Miles Morales becomes the Spider-Man of his reality and crosses paths with others from the multiverse.</p> 
-*/
-
 const moviesList = document.getElementById('movie-list');
 movies.forEach((movie) => {
     // Code for going through each movie and creating content for that movie
@@ -73,7 +63,6 @@ movies.forEach((movie) => {
     moviesList.appendChild(article);
 });
           
-
 const allRatingElems = document.querySelectorAll('article.movie p span');
 
 allRatingElems.forEach((elem) => {
