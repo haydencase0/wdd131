ee63703a886d14929674081a4cc4a02ba8803f56
haydencase0 <haymonkey04@gmail.com>
2026-03-26 09:54:13 -0600

    Updated Images

--- MissionPortfolio/Javascript/masonry.js
+++ MissionPortfolio/Javascript/masonry.js
@@ -1,228 +1,292 @@
-const grid = document.querySelector('.grid');
-
-const masonry = new Masonry(grid, {
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-item',
-    percentPosition: true
-});
 const gallery = [
     {
-        image: "../images/SceneryPics/TokyoTemple.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538856/TokyoTemple_mnkoom.jpg",
         description: "Front of Tokyo Temple",
         tags: ["Scenery"]
     },
     {
-        image: "../images/SceneryPics/carmountain.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538856/suwasakura_iali21.jpg",
         description: "Front of Tokyo Temple",
         tags: ["Scenery"]
     },
     {
-        image: "../images/SceneryPics/cloudyfuji.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538853/shizuokanight_ma5rl0.jpg",
         description: "Front of Tokyo Temple",
         tags: ["Scenery"]
     },
     {
-        image: "../images/SceneryPics/gifucastle.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538852/shinkansen_zodgbu.jpg",
         description: "Front of Tokyo Temple",
         tags: ["Scenery"]
     },
     {
-        image: "../images/SceneryPics/kanayamashrine.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538850/numazuskyline_mtitxu.jpg",
         description: "Front of Tokyo Temple",
         tags: ["Scenery"]
     },
     {
-        image: "../images/SceneryPics/nagoyacastle.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538848/numazubeach_n6uiud.jpg",
         description: "Front of Tokyo Temple",
         tags: ["Scenery"]
     },
     {
-        image: "../images/SceneryPics/nagoyasakura.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538848/nakedfuji_err24q.jpg",
         description: "Front of Tokyo Temple",
         tags: ["Scenery"]
     },
     {
-        image: "../images/SceneryPics/nagoyaskyline.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538846/nagoyaskyline_ndtwca.jpg",
         description: "Front of Tokyo Temple",
         tags: ["Scenery"]
     },
     {
-        image: "../images/SceneryPics/nakedfuji.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538844/nagoyasakura_hcxthk.jpg",
         description: "Front of Tokyo Temple",
         tags: ["Scenery"]
     },
     {
-        image: "../images/SceneryPics/numazubeach.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538844/nagoyacastle_x1elgm.jpg",
         description: "Front of Tokyo Temple",
         tags: ["Scenery"]
     },
     {
-        image: "../images/SceneryPics/numazuskyline.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538843/kanayamashrine_we3obw.jpg",
         description: "Front of Tokyo Temple",
         tags: ["Scenery"]
     },
     {
-        image: "../images/SceneryPics/shinkansen.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538841/gifucastle_blspdz.jpg",
         description: "Front of Tokyo Temple",
         tags: ["Scenery"]
     },
     {
-        image: "../images/SceneryPics/shizuokanight.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538840/cloudyfuji_w8zqqe.jpg",
         description: "Front of Tokyo Temple",
         tags: ["Scenery"]
     },
     {
-        image: "../images/SceneryPics/suwasakura.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538839/carmountain_n2jz0h.jpg",
         description: "Front of Tokyo Temple",
         tags: ["Scenery"]
     },
     {
-        image: "../images/PeoplePics/20birthday.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538627/toro_s79buj.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/christmascaroling.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538627/welcometojapan_ljktkj.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/dagangatyakiniku.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538627/thompson_jeyvvx.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/eldercook.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538622/takabataward_dtj2uf.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/filipinobrothers.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538621/suwayoungmen_zvdoyc.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/finalzoneconf.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538621/riosanddugar_uydepd.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/hurdle.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538621/saunders_xhaj9b.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/moore.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538620/petersen_p5jmb7.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/morrow.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538617/ouchi_fqegfc.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/mountainwmorrill.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538615/muranaka_jl4w7f.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/muranaka.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538614/morrow_xnhqra.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/ouchi.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538615/mountainwmorrill_kq0bg8.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/petersen.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538613/moore_yjz3px.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/riosanddugar.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538612/hurdle_gnwwan.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/saunders.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538611/finalzoneconf_ij5plo.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/suwayoungmen.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538611/filipinobrothers_vx7cx0.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/takabataward.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538610/eldercook_vavm17.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/thompson.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538610/dagangatyakiniku_v6wtzz.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/toro.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538609/christmascaroling_duta8z.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/welcometojapan.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538609/20birthday_r7pg2q.jpg",
         description: "Front of Tokyo Temple",
         tags: ["People"]
     },
     {
-        image: "../images/PeoplePics/dagangatyakiniku.jpg",
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538192/yakinikudonwmorrow_dawvtj.jpg",
         description: "Front of Tokyo Temple",
-        tags: ["People"]
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538178/pizzahut_ojsnyo.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538179/yakiniku_amqwev.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538179/wagyuburger_gyrgkw.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538179/sushi_pm8fni.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538179/suwaramen_fxzown.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538177/nagoyaburger_rheqy9.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538178/okonomiyaki_cohbg7.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538178/shizuokacurry_aszuuw.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538178/spicynotramen_bxizwu.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538177/nagoyaramen_ei2vqy.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538177/homemadeokonomiyaki_uplgo7.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538176/hotdog_u0hkuu.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538176/indocurry_cmtwiw.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538177/mountainicecream_glmrh4.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538177/bamboonoodles_cyyzne.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538176/classicjapaneselunch_obo1ir.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538176/fujipizza_bxjesa.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
+    },
+    {
+        image: "https://res.cloudinary.com/dasryldyl/image/upload/v1774538176/burrito_csdwjh.jpg",
+        description: "Front of Tokyo Temple",
+        tags: ["Food"]
     }
 ]
-/*
-const images = [
-    "../images/SceneryPics/TokyoTemple.jpg",
-    "../images/SceneryPics/TokyoTemple.jpg",
-    "../images/SceneryPics/TokyoTemple.jpg",
-    "../images/SceneryPics/TokyoTemple.jpg",
-    "../images/SceneryPics/TokyoTemple.jpg",
-    "../images/SceneryPics/TokyoTemple.jpg",
-    "../images/SceneryPics/TokyoTemple.jpg",
-    "../images/SceneryPics/TokyoTemple.jpg",
-    "../images/SceneryPics/TokyoTemple.jpg"
-];
-*/
+const grid = document.querySelector('.grid');
 
-function loadGallery(filterTag) {
+let currentIndex = 0;
+const batchSize = 15;
+let isLoading = false;
+let currentFilter = null;
+
+const masonry = new Masonry(grid, {
+    itemSelector: ".grid-item",
+    percentPosition: true,
+    gutter: 10
+});
+
+function loadGallery(filterTag = null) {
+    currentFilter = filterTag;
+    currentIndex = 0;
+
     grid.innerHTML = "";
+    masonry.reloadItems();
+    masonry.layout();
 
-    const filtered = gallery.filter(item => 
-        !filterTag || item.tags.includes(filterTag)
-    );
-
-    filtered.forEach(itemData => {
-        const item = document.createElement("div");
-        item.classList.add("grid-item");
-
-        const img = document.createElement("img");
-        img.src = itemData.image;
-
-        // 👇 CLICK EVENT FOR MODAL
-        img.addEventListener("click", () => {
-            openModal(itemData.image, itemData.description);
-        });
-
-        item.appendChild(img);
-        grid.appendChild(item);
-    });
-
-    imagesLoaded(grid, function () {
-        masonry.reloadItems();
-        masonry.layout();
-    });
+    loadMoreImages();
 }
 
 const modal = document.getElementById("modal");
@@ -240,36 +304,78 @@ closeBtn.addEventListener("click", () => {
     modal.classList.remove("active");
 });
 
-// Optional: click outside to close
 modal.addEventListener("click", (e) => {
     if (e.target === modal) {
         modal.classList.remove("active");
     }
 });
 
+function loadMoreImages() {
+    if (isLoading) return;
+    isLoading = true;
+
+    const filtered = currentFilter
+        ? gallery.filter(item => item.tags.includes(currentFilter))
+        : gallery;
+
+    const nextBatch = filtered.slice(currentIndex, currentIndex + batchSize);
+
+    if (nextBatch.length === 0) {
+        isLoading = false;
+        return;
+    }
+
+    nextBatch.forEach(itemData => {
+        const item = document.createElement("div");
+        item.classList.add("grid-item");
+        
+        const img = document.createElement("img");
+        img.src = itemData.image.replace(
+            "/upload/",
+            "/upload/w_500,q_auto,f_auto/"
+        );
+
+        img.loading = "lazy";        
+
+        img.addEventListener("click", () => {
+            openModal(itemData.image, itemData.description);
+        });
+
+        item.appendChild(img);
+        grid.appendChild(item);
+
+        masonry.appended(item);
+    });
+
+    currentIndex += batchSize;
+
+    imagesLoaded(grid, function () {
+        masonry.layout();
+        isLoading = false;
+    });
+}
+
+window.addEventListener("scroll", () => {
+    const scrollPosition = window.innerHeight + window.scrollY;
+    const pageHeight = document.body.offsetHeight;
+
+    if (scrollPosition >= pageHeight - 100) {
+        loadMoreImages();
+    }
+});
+
 const page = window.location.pathname;
 
-if (page.includes("index")){
-    loadGallery();
-}
-if (page.includes("people")) loadGallery("people");
-if (page.includes("food")) loadGallery("food");
-if (page.includes("scenery")) loadGallery("places");
-// images.forEach(src => {
-//     const item = document.createElement("div");
-//     item.classList.add("grid-item");
+document.addEventListener("DOMContentLoaded", () => {
+    const path = window.location.pathname;
 
-//     const img = document.createElement("img");
-//     img.src = src;
-
-//     item.appendChild(img);
-//     grid.appendChild(item);
-// });
-
-// Re-layout AFTER images load
-// imagesLoaded(grid, function () {
-//     new Masonry(grid, {
-//         itemSelector: '.grid-item',
-//         percentPosition: true
-//     });
-// });
+    if (path.includes("people")) {
+        loadGallery("People");
+    } else if (path.includes("food")) {
+        loadGallery("Food");
+    } else if (path.includes("scenery")) {
+        loadGallery("Places");
+    } else {
+        loadGallery();
+    }
+});
